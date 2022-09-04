5e851d01958a93d1c7ac968f84c0967731b9d5d8
JohnNixon6972 <johnbrightnixon@gmail.com>
2022-09-04 19:00:13 +0530

    Added more and more and more and moreee codes

--- todolost-v1/app.js
+++ todolost-v1/app.js
@@ -3,44 +3,30 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs")
 
 const app = express();
-app.set("view engine","ejs")
 
+var items = [];
+app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options ={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:       
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        
-    
-        default:
-            day = currentDay;
-            break;
-    }
-    res.render('list',{kindOfDay:day});
+    var day = today.toLocaleDateString("en-US",options)
+    console.log(day);
+   
+    res.render('list',{kindOfDay:day,newListItems:items});
 });
 
+app.post("/",function(req,res){
+    item = req.body.newItem
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
+});
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
